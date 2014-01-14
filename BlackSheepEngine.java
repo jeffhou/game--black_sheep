724b0fd1b9893b8335a792554eabadfa68e34c5e
Jeff Hou <j.king.hou@gmail.com>
2014-01-13 22:16:24 -0500

    Added Player Stats Screen as well as cheat keys for increasing exp.

--- BlackSheepEngine.java
+++ BlackSheepEngine.java
@@ -1,14 +1,18 @@
 package game_jh365;
+import java.awt.Color;
+
 import jgame.*;
 import jgame.platform.*;
-
+/** TODO
+ */
 public class BlackSheepEngine extends JGEngine{
 	public Integer[][] grid;
 	BSPlayer player;
+	BSPlayerObject playerObject;
 	public BlackSheepEngine(JGPoint size){
 		initEngine(size.x, size.y);
 	}
-
+	
 	@Override
 	public void initCanvas() {
 		setCanvasSettings(
@@ -23,14 +27,19 @@ public class BlackSheepEngine extends JGEngine{
 	public void initGame() {
 		setFrameRate(35, 2);
 		grid = new Integer[BlackSheepGame.WINDOW_TILE_LENGTH][BlackSheepGame.WINDOW_TILE_WIDTH];
-		player = new BSPlayer();
-		// TODO Auto-generated method stub
+		setGameState("InGame");
+	}
+	public void startInGame(){
+		if(player == null)
+			player = new BSPlayer();
+		playerObject = new BSPlayerObject();
+		playerObject.loadPlayer(player);
 	}
 	/** A simple timer. */
 	int gametimer=0;
 	/** Mouse position of previous frame. */
-	int prevmousex=0,prevmousey=0;
-	public void doFrame(){
+	//int prevmousex=0,prevmousey=0;
+	public void doFrameInGame(){
 		moveObjects(null, 0);
 		gametimer++;
 		//processKeys();
@@ -47,11 +56,89 @@ public class BlackSheepEngine extends JGEngine{
 			clearKey(KeyDown);
 			player.position_y += 1;
 		}
+		if(getKey('R')){
+			clearKey('R');
+			player.increaseRage(5);
+		}else if(getKey('G')){
+			clearKey('G');
+			player.increaseGrit(5);
+		}else if(getKey('B')){
+			clearKey('B');
+			player.increaseBrawn(5);
+		}
+		System.out.println(player.getRage());
+		this.setBGColor(new JGColor(255 - player.getRage(), 255 - player.getGrit(), 255 - player.getBrawn()));
+		
+		if(getKey(KeyEsc)){
+			clearKey(KeyEsc);
+			setGameState("PlayerStats");
+		}
 	}
-	class BSPlayer extends JGObject{
+	public void startPlayerStats(){
+		removeObjects(null, 0);
+	}
+	public void paintFramePlayerStats(){
+		drawString("Player Stats", pfWidth()/2, 40, 0, new JGFont("Arial", 0, 30), new JGColor(player.getRage(), player.getGrit(), player.getBrawn()));
+		drawString("RAGE", 80, 110, 1, new JGFont("Arial", 0, 18), new JGColor(player.getRage(), player.getGrit(), player.getBrawn()));
+		drawString("GRIT", 80, 160, 1, new JGFont("Arial", 0, 18), new JGColor(player.getRage(), player.getGrit(), player.getBrawn()));
+		drawString("BRAWN", 80, 210, 1, new JGFont("Arial", 0, 18), new JGColor(player.getRage(), player.getGrit(), player.getBrawn()));
+		drawRect(100, 108, player.getRage() / 2, 18, true, false, 1, JGColor.red);
+		drawRect(100, 158, player.getGrit() / 2, 18, true, false, 1, JGColor.green);
+		drawRect(100, 208, player.getBrawn() / 2, 18, true, false, 1, JGColor.blue);
+		drawString(player.getRage() + "", 240, 110, -1, new JGFont("Arial", 0, 18), new JGColor(player.getRage(), player.getGrit(), player.getBrawn()));
+		drawString(player.getGrit() + "", 240, 160, -1, new JGFont("Arial", 0, 18), new JGColor(player.getRage(), player.getGrit(), player.getBrawn()));
+		drawString(player.getBrawn() + "", 240, 210, -1, new JGFont("Arial", 0, 18), new JGColor(player.getRage(), player.getGrit(), player.getBrawn()));
+		//drawRect(100, 5, player.getRage(), 18, true, false);
+	}
+	public void doFramePlayerStats(){
+		if(getKey(KeyEsc)){
+			clearKey(KeyEsc);
+			setGameState("InGame");
+		}
+	}
+	class BSPlayer{
 		int position_x;
 		int position_y;
+		
+		int statRage;
+		int statGrit;
+		int statBrawn;
 		BSPlayer(){
+			position_x = 8;
+			position_y = 1;
+			
+			statRage = 5;
+			statGrit = 5;
+			statBrawn = 5;
+		}
+		public void increaseRage(int increaseAmt){
+			statRage += increaseAmt;
+		}
+		public void increaseGrit(int increaseAmt){
+			statGrit += increaseAmt;
+		}
+		public void increaseBrawn(int increaseAmt){
+			statBrawn += increaseAmt;
+		}
+		public int getRage(){
+			return statRage;
+		}
+		public int getGrit(){
+			return statGrit;
+		}
+		public int getBrawn(){
+			return statBrawn;
+		}
+		public int getPositionX(){
+			return position_x;
+		}
+		public int getPositionY(){
+			return position_y;
+		}
+	}
+	class BSPlayerObject extends JGObject{
+		BSPlayer player;
+		BSPlayerObject(){
 			super(
 				"Player",// name by which the object is known
 				true,
@@ -63,15 +150,16 @@ public class BlackSheepEngine extends JGEngine{
 			);
 			xspeed = 0;
 			yspeed = 0;
-			position_x = 8;
-			position_y = 1;
+		}
+		public void loadPlayer(BSPlayer loadedPlayer){
+			player = loadedPlayer;
 		}
 		public void move(){
-			x = position_x * BlackSheepGame.TILE_LENGTH;
-			y = position_y * BlackSheepGame.TILE_WIDTH;
+			x = player.position_x * BlackSheepGame.TILE_LENGTH;
+			y = player.position_y * BlackSheepGame.TILE_WIDTH;
 		}
 		public void paint(){
-			setColor(new JGColor(220, 220, 220));
+			setColor(new JGColor(player.getRage(), player.getGrit(), player.getBrawn()));
 			drawOval(x,y,16,16,true,false);
 		}
 	}
